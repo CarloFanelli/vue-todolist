dc3f7fe1055a5a0c8dcb1e05038c312d3229bafa
CarloFanelli <carlo.fanelli.sax@gmail.com>
2023-09-13 14:26:51 +0200

    add list of things

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,42 @@
+/* 
+Descrizione: Rifare l’esercizio della to do list. Questa volta però ogni todo sarà un oggetto, formato da due proprietà:
+text, una stringa che indica il testo del todo
+done, un booleano (true/false) che indica se il todo è stato fatto oppure no
+ */
+
 const { createApp } = Vue
 
   createApp({
     data() {
       return {
+        list : [
+            {
+                text : 'comprare l\'acqua',
+                done : false
+            },
+            {
+                text : 'andare dal fruttivendolo',
+                done : false
+            },
+            {
+                text : 'dare da bere alle piante',
+                done : false
+            },
+            {
+                text : 'preparare una torta',
+                done : false
+            },
+            {
+                text : 'visitare un castello',
+                done : false
+            },
+            {
+                text : 'portare fuori il cane',
+                done : false
+            },
+        ]
 
     }
     }
   }).mount('#app')
+
